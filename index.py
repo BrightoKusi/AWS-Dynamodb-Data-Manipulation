3799dcd6c3fdb5e95d1e44740e07e4c5d5f6cd1a
unknown <brightokusi@gmail.com>
2024-04-03 02:34:42 +0000

    update records of table

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import boto3
 import decimal
 
+
+from botocore.exceptions import ClientError
+
 def get_dynamodb_client():
     dynamodb = boto3.client("dynamodb", region_name="eu-west-1", endpoint_url="http://localhost:8000")
     """ :type : pyboto3.dynamodb """
@@ -72,6 +75,51 @@ def put_item_on_table():
     except Exception as error:
         print(error)
 
+
+def update_item_on_table():
+    response = get_dynamodb_resource().Table("Movies").update_item(
+        Key={
+            'year': 2015,
+            'title': 'The Big New Movie'
+        },
+        UpdateExpression="set info.rating = :r, info.plot = :p, info.actors = :a",
+        ExpressionAttributeValues={
+            ':r': decimal.Decimal(3.5),
+            ':p': "Everything happens all at once",
+            ':a': ["Larry", "Moe", "David"]
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    print("Updating existing movie was success!")
+    print(str(response))
+
+
+def conditionally_update_an_item():
+    try:
+        respone = get_dynamodb_resource().Table("Movies").update_item(
+            Key={
+                'year': 2015,
+                'title': 'The Big New Movie'
+            },
+            UpdateExpression="remove info.actors[0]",
+            ConditionExpression="size(info.actors) >= :num",
+            ExpressionAttributeValues={
+                ':num': 3
+            },
+            ReturnValues="UPDATED_NEW"
+        )
+    except ClientError as error:
+        if error.response['Error']['Code'] == "ConditionalCheckFailedException":
+            print(error.response['Error']['Message'])
+        else:
+            raise
+    else:
+        print("Updated item on table conditionally!")
+        print(str(respone))
+
 if __name__ == '__main__':
     # create_table()
-    put_item_on_table()
+    # put_item_on_table()
+    update_item_on_table()
+    conditionally_update_an_item()
